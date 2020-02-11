bbc77f561f2dd15b91fcfa6fa1618a8cc11689f4
Nat Elliott-Green <natelliottgreen34@gmail.com>
2020-02-11 11:45:09 +0000

    Tests passing for counting multiple visits to sites and correct order

--- spec/webserver_parser_spec.rb
+++ spec/webserver_parser_spec.rb
@@ -8,7 +8,7 @@ describe WebserverParser do
   let(:multiple_visits_logfile) { 'spec/fixtures/webserver_multiple_visits_example.log' }
   subject(:multiple_visit_parser) { described_class.new(multiple_visits_logfile) }
 
-  let(:single_unique_visit_logfile) { 'spec/fixtures/webserver_single_unique_visit_example.log' }
+  let(:single_unique_visit_logfile) { 'spec/fixtures/webserver_unique_visit_example.log' }
   subject(:single_unique_visit_parser) { described_class.new(single_unique_visit_logfile) }
 
   context "listing of pages by most views" do
@@ -21,7 +21,7 @@ describe WebserverParser do
       expect(multiple_visit_parser.most_views).to include('/about/2 1 visits')
     end
 
-    it "orders the visits from highest number of visits to lowest" do
+    it "orders the visits from highest to lowest" do
       expect(multiple_visit_parser.most_views[0]).to eq('/help_page/1 3 visits')
       expect(multiple_visit_parser.most_views[1]).to eq('/home 2 visits')
     end
@@ -29,7 +29,17 @@ describe WebserverParser do
 
   context "listing of pages by unique views" do
     it "returns '/contact 1 unique visits'" do
-      expect(single_unique_visit_parser.unique_views[0]).to eq('/contact 1 unique visits')
+      expect(single_unique_visit_parser.unique_views[1]).to eq('/contact 1 unique visits')
+    end
+
+    it "returns the correct count for multiple unique visits" do
+      expect(single_unique_visit_parser.unique_views).to include('/home 2 unique visits')
+      expect(single_unique_visit_parser.unique_views).to include('/contact 1 unique visits')
+    end
+
+    it "orders the unique visits from highest to lowest" do
+      expect(single_unique_visit_parser.unique_views[0]).to eq('/home 2 unique visits')
+      expect(single_unique_visit_parser.unique_views[1]).to eq('/contact 1 unique visits')
     end
   end
 
